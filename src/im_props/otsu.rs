3266a7bfacfdb89e6d6f550c7d979ccae4d9501b
n1k40 <vowlesn@hotmail.co.uk>
2016-01-08 16:45:22 +0000

    setup for loop

--- src/im_props/otsu.rs
+++ src/im_props/otsu.rs
@@ -8,6 +8,9 @@ trait otsu_threshold{
 impl otsu_threshold for image::DynamicImage{
     fn get_otsu(&self) -> i32{
         let histogram = Histogram::new(self);
+        for threshold in 0..256{
+            
+        }
         return 1;
     }
 }
