9a13f6d5f68123640b6fbab4f2bb3c8dc5b91078
n1k40 <vowlesn@hotmail.co.uk>
2016-01-08 16:50:11 +0000

    comment of background thresh

--- src/im_props/otsu.rs
+++ src/im_props/otsu.rs
@@ -9,6 +9,9 @@ impl otsu_threshold for image::DynamicImage{
     fn get_otsu(&self) -> i32{
         let histogram = Histogram::new(self);
         for threshold in 0..256{
+            //let weight = sum of values less than threshold / (h * w)
+            //mean = sum of (value * count) / # values
+            //variance 
             
         }
         return 1;
