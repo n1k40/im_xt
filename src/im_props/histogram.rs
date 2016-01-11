285510553403835772c952cc525294341fb10d36
n1k40 <vowlesn@hotmail.co.uk>
2016-01-11 08:40:38 +0000

    made it so underneath histogram is a btree map so can just take an amount for values under threshold as its ordered

--- src/im_props/histogram.rs
+++ src/im_props/histogram.rs
@@ -1,23 +1,22 @@
 extern crate image;
 
 use self::image::DynamicImage;
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 
 pub struct Histogram{
-	_hashmap: HashMap<u8, i32>,
+	_hashmap: BTreeMap<u8, i32>,
 }
 
 impl Histogram {
 	pub fn new(image : &DynamicImage) -> Histogram{
-		let mut pixel_count = HashMap::new();
-		'outer : for pixel in image.raw_pixels(){
-			if !pixel_count.contains_key(&pixel){
-				pixel_count.insert(pixel, 1);
-				continue 'outer;
-			}
+        let mut pixel_count = BTreeMap::new();
+        for key in 0..255{
+            pixel_count.insert(key, 0);
+        }
+		for pixel in image.raw_pixels(){			
 			match pixel_count.get_mut(&pixel){
 				Some(count) => *count+=1,
-				None => continue 'outer,
+				None => continue,
 			};
 			//let v = pixel_count.get(&pixel);	
 			//println!("pixel is: {:?} count is: {:?}", pixel, v);			
@@ -32,12 +31,13 @@ impl Histogram {
         match self._hashmap.iter().max_by(|&(_, v)| v) {
             Some((key, _)) => *key,
             None => panic!("nothing found in histogram!"),
-        }		
+        }
 	}
     
-    pub fn get_values_under_threshold(threshold : i32) -> Vec<(u8, i32)>{
-        let mut v: Vec<(u8, i32)> = Vec::new();
-        v
+    pub fn get_values_under_threshold(&self, threshold : usize) -> Vec<(&u8, &i32)> {
+        self._hashmap.iter()
+                .take(threshold)
+                .collect()                       
     }	
 }
 
@@ -66,4 +66,23 @@ mod tests {
 		println!("mode is: {:?}", mode);
 		assert_eq!(100, mode);
 	}
+    #[test]
+	fn values_under_threshold(){
+        let height = 512;
+        let width = 512;
+		let mut luma =  DynamicImage::new_luma8(width, height);
+        if let DynamicImage::ImageLuma8(ref mut luma) = luma {
+            for row in 0..width{
+                for column in 0..height{
+                    luma.get_pixel_mut(row, column).data = [100];
+                }
+            }
+        }
+		
+		let hist = Histogram::new(&luma);
+        let threshold = 10;
+        let x = hist.get_values_under_threshold(threshold);
+        assert_eq!(threshold, x.len());
+        
+    }
 }
